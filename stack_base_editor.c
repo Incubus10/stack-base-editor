5de4d875a745cdb3835e98a0ae06bde38c9c0488
Incubus10 <78927240+Incubus10@users.noreply.github.com>
2021-11-15 12:19:28 +0530

    Update stack_base_editor.c

--- stack_base_editor.c
+++ stack_base_editor.c
@@ -1,60 +1,141 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#define MAX 10		
 
-struct stack{
-    struct stack *left;
-    struct stack *right;
-    int size;
-    int top;
-    int * arr;
-};
+int top = -1;
 
-int isEmpty(struct stack *s ){
-    if(s->top == -1){
-        return 1;
-    }
-    else{
-    return 0;
+char stack[MAX];
+char stackA[MAX];
+int sizeA=0,sizeB=0,sizeC=0;
+
+char push (char *stack,char val) 
+{
+
+if (top == MAX - 1){
+printf ("\nStack is full!!");
+}
+else{
+
+top = top + 1;
+stack[top] = val;
+}
+}
+
+
+char pop (char *stack) {
+if (top == -1){
+printf ("\nStack is empty!!");
+}
+else{
+    char val=stack[top];
+top = top - 1;
+return val;
     }
 }
 
-int isFull(struct stack * s){
-    if(s->top==s->size-1){
-        return 1;
-    }
 
-    else{
-        return 0;
-    }
 
-} 
+void display () {
+int i;
+if (top == -1){
+printf ("\nStack is empty!!");
+}
+else{
+printf ("\nStack is...\n");
+for (i = 0; i <= top; i++)
+printf ("%c", stack[i]);
 
-void push(struct stack * s, int data){
-    if(isFull(s)){
-        printf("full stack");
-    }
-    else{
-        s->top++;
-        s->arr[s->top]=data;
-    }
+}
 }
 
-int pop(struct stack * s){
-    if(isEmpty(s)){
-        printf("empty stack");
-        return -1;
+char moveleft(int index){
+    char x;
+    char y[20]; 
+    
+    
+    for(int i=0;i<index;i++){
+        y[i]=pop(stack);
+        
     }
-    else{
-        int val=s->arr[s->top];
-        s->top--;
-        return 1;
+    
+        push(stack,'^');
+        for(int i=index-1;i>-1;i--){
+        push(stack,y[i]);
+        }
+        
+    
+    
+}
+char moveright(int index){
+    char y[20];
+    char x;
+    for(int i=0;i<index;i++){
+        if(stack[i]!='^'){
+        y[i]=pop(stack);
+        
     }
+    x=pop(stack);
+    
+    }
+     for(int i=index;i>-1;i--){
+        push(stack,y[i]);
+        printf(" %c",y[i]);
+         }
+         
+        push(stack,x);
+    //     for(int i=index;i<strlen(stack);i++){
+    //         push(stack,y[i]);
+    //     }
+    
 }
 
-int stackTOp(struct stack *s){
-    return s->arr[s->top];
+
+ 
+int main () {
+int ch;
+int index;
+while (1)
+    {
+printf ("\n*** Stack Menu ***");
+printf ("\n\n1.Insert\n2.Backspace\n3.Display\n4.Moveleft\n5.Moveright\n5.Exit");
+printf ("\n\nEnter your choice(1-4):");
+scanf (" %d", &ch);
+
+if(ch==1) {
+    char val;
+    printf ("\nEnter element to add:");
+scanf (" %c", &val);
+	  push (stack,val);
+	  
 }
+
+else if(ch==2){
+	  pop (stack);
+}
+else if(ch==3){
+	  display ();
+}
+else if(ch==4){
+    printf("Enter the index : ");
+    scanf(" %d",&index);
+    moveleft(index);
+}
+else if(ch==5){
+	  printf("Enter the index : ");
+    scanf(" %d",&index);
+    moveright(index);
+}
+else if(ch==6){
+    exit(0);
+}
+else{
+	  printf ("\nWrong Choice!!");
+}
+
+}
+}
+
 
 void check(struct stack * left,struct stack * right){
     struct stack *l=(struct stack *)malloc(sizeof(struct stack));
